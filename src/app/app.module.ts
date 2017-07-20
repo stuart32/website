eb94c08ebfcb5becc29d6e0c1b25fc9da4607958
Stuart Fraser <srf32@hw.ac.uk>
2017-07-20 22:24:45 +0100

    Added first route for 'about'; menu and about components (menu with css)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu.component';
+import { AboutComponent } from './about.component';
+import { AppRoutingModule }     from './app-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    AboutComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
